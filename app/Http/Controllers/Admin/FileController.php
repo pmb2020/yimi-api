c7e4fe25b7123692470d3ca70e164680338dce17
wangbo <pmb2020@163.com>
2023-04-16 11:29:35 +0800

    +

--- app/Http/Controllers/Admin/FileController.php
+++ app/Http/Controllers/Admin/FileController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\FileRequest;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rules\File;
 
 class FileController extends Controller
 {
