298b6966d01f3ff16e86f143e69f86a0b619314e
wangbo <pmb2020@163.com>
2023-04-23 16:18:23 +0800

    +

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AdminSeeder::class
+            AdminSeeder::class,
+            MenuSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
